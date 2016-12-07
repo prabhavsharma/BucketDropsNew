b158622823720f449b45d6baee6072ee439b241e
Prabhav Sharma <prabhav.sharma@cerner.com>
2016-12-07 15:46:03 -0600

    main activity

--- app/src/main/java/com/developer/android/bucketdrops/MainActivity.java
+++ app/src/main/java/com/developer/android/bucketdrops/MainActivity.java
@@ -3,7 +3,10 @@ package com.developer.android.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -11,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView background;
     Toolbar mToolbar;
+    Button mBtnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Button",Toast.LENGTH_SHORT).show();
+            }
+        });
         setSupportActionBar(mToolbar);
         initBackground();
     }
@@ -24,4 +35,5 @@ public class MainActivity extends AppCompatActivity {
         background = (ImageView) findViewById(R.id.iv_background);
         Glide.with(this).load(R.drawable.background).centerCrop().into(background);
     }
+
 }
